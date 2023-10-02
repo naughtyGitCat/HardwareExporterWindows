5ffce0944830a8b0c122310aa410a595f65333ca
naughtyGitCat <psyduck007@outlook.com>
2023-10-02 18:17:46 +0800

    add  ref

--- src/HardwareExporterWindows/Program.cs
+++ src/HardwareExporterWindows/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// https://stackoverflow.com/questions/7764088/net-console-application-as-windows-service
 builder.Host.UseWindowsService(c=>c.ServiceName="HardwareExporter");
 //builder.Services.AddHostedService<Y>();
 
